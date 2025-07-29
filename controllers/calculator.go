7ad168f90f4c421d876964b889f561c0a14c474a
Pedro Leite <pedrohleite2005@gmail.com>
2025-07-29 12:10:57 -0300

    feat: adicionar lógica para calcular

--- controllers/calculator.go
+++ controllers/calculator.go
@@ -1,7 +1,51 @@
 package controllers
 
-import "calculadora-api/models"
+import (
+	"calculadora-api/models"
+	"fmt"
+)
+
+var (
+	resultado float64
+	mensagem  string
+	erro      bool
+)
 
 func CalcularOperacao(num1, num2 float64, operador string) (models.OperationResponse, bool) {
-	return nil, false
+	switch operador {
+	case "+":
+		resultado = num1 + num2
+		mensagem = fmt.Sprintf("A soma de %2.f e %2.f é %2.f", num1, num2, resultado)
+	case "-":
+		resultado = num1 - num2
+		mensagem = fmt.Sprintf("A subtração de %2.f e %2.f é %2.f", num1, num2, resultado)
+	case "*":
+		resultado = num1 * num2
+		mensagem = fmt.Sprintf("A multiplicação de %2.f e %2.f é %2.f", num1, num2, resultado)
+	case "/":
+		if num2 == 0 {
+			mensagem = "Não é possível dividir um número por zero."
+			erro = true
+		} else {
+			resultado = num1 / num2
+			mensagem = fmt.Sprintf("A divisão de %2.f e %2.f é %2.f", num1, num2, resultado)
+		}
+	case "%":
+		if num2 == 0 {
+			mensagem = "Não é possível calcular o módulo com divisor zero."
+			erro = true
+		} else {
+			resultado = float64(int(num1) % int(num2))
+			mensagem = fmt.Sprintf("O módulo de %2.f e %2.f é %2.f", num1, num2, resultado)
+		}
+	default:
+		mensagem = "Operação desconhecida."
+		erro = true
+	}
+
+	return models.OperationResponse{
+		Resultado: resultado,
+		Mensagem:  mensagem,
+		Erro:      erro,
+	}, erro
 }
